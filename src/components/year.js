6fdad70fb5192122134d9aab3dc243de13430179
Alpii <alparslanabdik@gmail.com>
2024-03-01 00:57:58 +0100

    Update year.js

--- src/components/year.js
+++ src/components/year.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer>
-      <p>&copy; {currentYear} All rights reserved.</p>
+      <p>&copy; {currentYear} All Rights Reserved.</p>
     </footer>
   );
 };
